c82b6805fae38b7f75b656f9b82d52f82e9a18dd
Bedknobs <noeliareysilva@gmail.com>
2024-08-16 10:39:07 +0200

    Added Book and Comment models

--- backend/src/main/java/com/bookhive/backend/domain/Book.java
+++ backend/src/main/java/com/bookhive/backend/domain/Book.java
@@ -1,17 +1,12 @@
 package com.bookhive.backend.domain;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.ManyToMany;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,17 +15,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AppUser {
+public class Book {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String username;
-    private String password;
+    private String author;
+    private Integer release_year;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    private Collection<Role> roles = new ArrayList<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
     private List<Comment> comments;
+
 }
---
+++ backend/src/main/java/com/bookhive/backend/domain/Comment.java
@@ -0,0 +1,29 @@
+package com.bookhive.backend.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private Integer rating;
+
+    @ManyToOne
+    private Book book;
+
+    @ManyToOne
+    private AppUser user;
+}
